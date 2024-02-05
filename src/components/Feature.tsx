4f0bdb9436435d4520e83c7e1dbf6734c0044964
murgatt <12082276+murgatt@users.noreply.github.com>
2024-02-05 19:54:13 +0100

    add "New in v2" badge

--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import { Badge } from '@/components/ui/Badge';
 
 type FeatureProps = {
   children: React.ReactNode;
   icon: React.ReactNode;
+  isNew?: boolean;
 };
 
-export const Feature = ({ children, icon }: FeatureProps) => {
+export const Feature = ({ children, icon, isNew }: FeatureProps) => {
   return (
     <li className="flex flex-col items-start gap-2">
       <span className="flex items-center gap-2 paragraph">
         <span className="shrink-0">{icon}</span>
         {children}
+        {isNew && <Badge className="whitespace-nowrap">New in v2</Badge>}
       </span>
     </li>
   );
--- src/components/FeatureList.tsx
+++ src/components/FeatureList.tsx
@@ -17,14 +17,22 @@ export const FeatureList = () => {
       <h3 className="mb-4 w-full title-h3">Features</h3>
       <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         <Feature icon={<AudioLinesIcon size="20" />}>Convert audio codecs</Feature>
-        <Feature icon={<SubtitlesIcon size="20" />}>Convert subtitle</Feature>
+        <Feature icon={<SubtitlesIcon size="20" />} isNew>
+          Convert subtitle
+        </Feature>
         <Feature icon={<FileStackIcon size="20" />}>Convert multiple files at the same time</Feature>
         <Feature icon={<FileCogIcon size="20" />}>Conversion settings</Feature>
         <Feature icon={<FilePenLineIcon size="20" />}>View, edit and remove streams</Feature>
         <Feature icon={<SunMoonIcon size="20" />}>Light & dark themes</Feature>
-        <Feature icon={<BellIcon size="20" />}>Conversion notification</Feature>
-        <Feature icon={<SettingsIcon size="20" />}>Application settings</Feature>
-        <Feature icon={<TerminalIcon size="20" />}>Display spawned FFmpeg command</Feature>
+        <Feature icon={<BellIcon size="20" />} isNew>
+          Conversion notification
+        </Feature>
+        <Feature icon={<SettingsIcon size="20" />} isNew>
+          Application settings
+        </Feature>
+        <Feature icon={<TerminalIcon size="20" />} isNew>
+          Display spawned FFmpeg command
+        </Feature>
       </ul>
     </section>
   );
